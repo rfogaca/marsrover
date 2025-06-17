bb10e3f67865037757fd0194f485686d82d89f46
Rodrigo Fogaça <rodrigofogacars@gmail.com>
2025-06-17 14:42:21 -0300

    Criação do CommandFactory

---
+++ MarsRover.ConsoleApplication/Commands/CommandFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MarsRover.Commands
+{
+    public static class CommandFactory
+    {
+        public static List<ICommand> CreateCommandSequence(string commandString)
+        {
+            var commands = new List<ICommand>();
+
+            foreach (char c in commandString)
+            {
+                commands.Add(CreateCommand(c));
+            }
+
+            return commands;
+        }
+
+        public static ICommand CreateCommand(char commandChar)
+        {
+            return commandChar switch
+            {
+                'L' => new RotateLeftCommand(),
+                'R' => new RotateRightCommand(),
+                'M' => new MoveCommand(),
+                _ => throw new ArgumentException($"Comando inválido: {commandChar}")
+            };
+        }
+    }
+}
