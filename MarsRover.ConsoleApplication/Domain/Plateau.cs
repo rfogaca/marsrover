c2bcea00f8aa67798568bc4f46f5b78b13337eb7
Rodrigo Fogaça <rodrigofogacars@gmail.com>
2025-06-17 14:49:27 -0300

    Criação da classe de domínio Plateau

---
+++ MarsRover.ConsoleApplication/Domain/Plateau.cs
@@ -0,0 +1,40 @@
+﻿namespace MarsRover.Domain
+{
+    public class Plateau
+    {
+        public int MaxX { get; }
+        public int MaxY { get; }
+
+        private readonly HashSet<Position> occupiedPositions = new();
+
+        public Plateau(int maxX, int maxY)
+        {
+            if (maxX < 0 || maxY < 0)
+                throw new ArgumentException("Coordenadas máximas devem ser não-negativas.");
+
+            MaxX = maxX;
+            MaxY = maxY;
+        }
+
+        public bool IsWithinBounds(Position position)
+        {
+            return position.X >= 0 && position.X <= MaxX &&
+                   position.Y >= 0 && position.Y <= MaxY;
+        }
+
+        public bool IsOccupied(Position position)
+        {
+            return occupiedPositions.Contains(position);
+        }
+
+        public void Occupy(Position position)
+        {
+            occupiedPositions.Add(position);
+        }
+
+        public void Vacate(Position position)
+        {
+            occupiedPositions.Remove(position);
+        }
+    }
+}
